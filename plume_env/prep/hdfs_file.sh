f0bd2e4175f8b3a1c39f6ae71b07a84ba0cc74d9
Murugesan Guruswamy <muru@plumewifi.com>
2019-05-05 09:33:42 -0700

    move hdfs copy to hdfs_file

--- plume_env/prep/hdfs_file.sh
+++ plume_env/prep/hdfs_file.sh
@@ -17,12 +17,35 @@ hadoop fs -chmod g+w /tmp
 hadoop fs -chmod g+w /user/hive/warehouse
 
 # mongo jars
-hadoop fs -put $MONGO_HOME/* /home/spark_lib/
+echo hadoop fs -put $MONGO_HOME/* /home/spark_lib/
+#hadoop fs -put $MONGO_HOME/* /home/spark_lib/
 
 # aws s3 jars
-hadoop fs -put $HADOOP_AWS_HOME/* /home/spark_lib/
+echo hadoop fs -put $HADOOP_AWS_HOME/* /home/spark_lib/
+#hadoop fs -put $HADOOP_AWS_HOME/* /home/spark_lib/
 
 # avro
 wget http://central.maven.org/maven2/com/databricks/spark-avro_2.11/4.0.0/spark-avro_2.11-4.0.0.jar
 hadoop fs -put spark-avro_2.11-4.0.0.jar /home/spark_lib/
 rm spark-avro_2.11-4.0.0.jar
+
+
+
+hadoop fs -mkdir /griffin
+hadoop fs -mkdir /griffin/json
+hadoop fs -mkdir /griffin/persist
+hadoop fs -mkdir /griffin/checkpoint
+
+hadoop fs -mkdir /griffin/data
+hadoop fs -mkdir /griffin/data/batch
+
+#measure file
+hadoop fs -put measure/griffin-measure.jar /griffin/
+
+
+#service
+sed s/ES_HOSTNAME/$ES_HOSTNAME/ /root/service/config/application.properties.template > /root/service/config/application.properties
+sed -i 's/HOSTNAME/$HOSTNAME/g' /root/service/config/application.properties
+
+#json
+#hadoop fs -put json/*.json /griffin/json/
